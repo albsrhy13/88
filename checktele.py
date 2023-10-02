b692c3dbb596b7b2e885e71da4a7a61c6292c2e4
albsrhy13 <142347858+albsrhy13@users.noreply.github.com>
2023-10-02 13:32:53 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -1,6 +1,4 @@
-import datetime
 import random
-import threading
 import asyncio
 import telethon
 from telethon import events
@@ -10,11 +8,12 @@ from telethon.sync import functions
 from user_agent import generate_user_agent
 import requests
 from user_agent import *
-from help import *
+from new import *
 from config import *
 from threading import Thread
+import datetime
 
-a = 'qwertyuiopasdfghjklzxcvbnm'
+a = 'qwertyuiopassdfghjklzxcvbnm'
 b = '1234567890'
 e = 'qwertyuiopassdfghjklzxcvbnm1234567890'
 
@@ -24,8 +23,6 @@ isauto = ["off"]
 with open("banned.txt", "r") as f:
     f = f.read().split()
     banned.append(f)
-
-current_datetime = datetime.datetime.now()
 
 que = Queue()
 
@@ -47,7 +44,7 @@ def check_user(username):
 def gen_user(choice):
     if choice == "1":
         c = random.choices(a)
-        d = random.choices(e)
+        d = random.choices(a)
         s = random.choices(e)
         f = [c[0], "_", d[0], "_", s[0]]
         username = ''.join(f)
@@ -61,53 +58,41 @@ def gen_user(choice):
             pass
     if choice == "2":
         c = random.choices(a)
-        d = random.choices(a)
+        d = random.choices(b)
         s = random.choices(e)
-        f = [c[0], "_", d[0], "_", s[0]]
+        f = [c[0], s[0]]
+        random.shuffle(f)
         username = ''.join(f)
+        username = username+'bot'
         if username in banned[0]:
             c = random.choices(a)
             d = random.choices(b)
             s = random.choices(e)
-            f = [c[0], "_", d[0], "_", s[0]]
+            f = [c[0], s[0]]
+            random.shuffle(f)
             username = ''.join(f)
+            username = username+'bot'
         else:
             pass
     if choice == "3":
-        c = random.choices(b)
+        c = random.choices(a)
         d = random.choices(b)
-        f = [c[0], d[0]]
+        s = random.choices(e)
+        f = [c[0], s[0], d[0]]
         random.shuffle(f)
         username = ''.join(f)
-        username = 'vip'+username
+        username = username+'bot'
         if username in banned[0]:
-            c = random.choices(b)
+            c = random.choices(a)
             d = random.choices(b)
-            f = [c[0], d[0]]
+            s = random.choices(e)
+            f = [c[0], s[0], d[0]]
             random.shuffle(f)
             username = ''.join(f)
-            username = 'vip'+username
+            username = username+'bot'
         else:
             pass
     if choice == "4":
-        c = random.choices(b)
-        d = random.choices(b)
-        s = random.choices(b)
-        f = [c[0], d[0], s[0]]
-        random.shuffle(f)
-        username = ''.join(f)
-        username = 'vip'+username
-        if username in banned[0]:
-            c = random.choices(b)
-            d = random.choices(b)
-            s = random.choices(b)
-            f = [c[0], d[0], s[0]]
-            random.shuffle(f)
-            username = ''.join(f)
-            username = 'vip'+username
-        else:
-            pass
-    if choice == "5":
         c = d = random.choices(a)
         d = random.choices(b)
         f = [c[0], d[0], c[0], c[0], c[0]]
@@ -121,21 +106,21 @@ def gen_user(choice):
             username = ''.join(f)
         else:
             pass
-    if choice == "6":
+    if choice == "5":
         c = d = random.choices(a)
         d = random.choices(b)
-        f = [c[0], d[0], c[0], c[0], d[0]]
+        f = [c[0], d[0], d[0], c[0], d[0]]
         random.shuffle(f)
         username = ''.join(f)
         if username in banned[0]:
             c = d = random.choices(a)
             d = random.choices(b)
-            f = [c[0], d[0], c[0], c[0], d[0]]
+            f = [c[0], d[0], d[0], c[0], d[0]]
             random.shuffle(f)
             username = ''.join(f)
         else:
             pass
-    if choice == "7":
+    if choice == "6":
         c = d = random.choices(a)
         d = random.choices(b)
         f = [c[0], c[0], c[0], c[0], c[0], d[0]]
@@ -149,21 +134,21 @@ def gen_user(choice):
             username = ''.join(f)
         else:
             pass
-    if choice == "8":
+    if choice == "7":
         c = d = random.choices(a)
         d = random.choices(b)
-        f = [c[0], d[0], c[0], c[0], c[0], d[0]]
+        f = [c[0], d[0], c[0], c[0], c[0], s[0]]
         random.shuffle(f)
         username = ''.join(f)
         if username in banned[0]:
             c = d = random.choices(a)
             d = random.choices(b)
-            f = [c[0], d[0], c[0], c[0], c[0], d[0]]
+            f = [c[0], d[0], c[0], c[0], c[0], s[0]]
             random.shuffle(f)
             username = ''.join(f)
         else:
             pass
-    if choice == "9":
+    if choice == "8":
         c = d = random.choices(a)
         d = random.choices(b)
         f = [c[0], c[0], c[0], c[0], d[0], c[0], c[0]]
@@ -177,63 +162,106 @@ def gen_user(choice):
             username = ''.join(f)
         else:
             pass
+    if choice == "9":
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(e)
+        f = [c[0], c[0], d[0], d[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        username = username+'bot'
+        if username in banned[0]:
+            c = random.choices(a)
+            d = random.choices(b)
+            s = random.choices(e)
+            f = [c[0], c[0], d[0], d[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+            username = username+'bot'
+        else:
+            pass
     if choice == "10":
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(e)
+        f = [c[0], c[0], d[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        username = username+'bot'
+        if username in banned[0]:
+            c = random.choices(a)
+            d = random.choices(b)
+            s = random.choices(e)
+            f = [c[0], c[0], d[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+            username = username+'bot'
+        else:
+            pass
+    if choice == "11":
         c = d = random.choices(a)
         d = random.choices(b)
-        f = [c[0], d[0], "_", c[0], c[0]]
+        f = [c[0], c[0], c[0], d[0], d[0], d[0]]
         random.shuffle(f)
         username = ''.join(f)
         if username in banned[0]:
             c = d = random.choices(a)
             d = random.choices(b)
-            f = [c[0], d[0], "_", c[0], c[0]]
+            f = [c[0], c[0], c[0], d[0], d[0], d[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+        else:
+            pass
+    if choice == "12":
+        c = d = random.choices(a)
+        d = random.choices(b)
+        f = [c[0], "_", c[0], "_", c[0], "_", d[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(b)
+            f = [c[0], "_", c[0], "_", c[0], "_", d[0]]
             random.shuffle(f)
             username = ''.join(f)
         else:
             pass
     return username
 
-@sython.on(events.NewMessage(outgoing=True, pattern=r"\.تشيكر"))
+@turbo.on(events.NewMessage(outgoing=True, pattern=r"تشيكر"))
 async def _(event):
     if ispay2[0] == "yes":
         await event.edit(tele_checker)
-        
-@sython.on(events.NewMessage(outgoing=True, pattern=r"\.اليوزرات المبندة"))
-async def _(event):
-    if ispay2[0] == "yes":
-        await sython.send_file(event.chat_id, 'banned.txt')
-
-
-@sython.on(events.NewMessage(outgoing=True, pattern=r"\.الانواع"))
+    
+@turbo.on(events.NewMessage(outgoing=True, pattern=r"الانواع"))
 async def _(event):
     if ispay2[0] == "yes":
         await event.edit(tele_checker2)
-# صيد عدد نوع قناة
-
-
-@sython.on(events.NewMessage(outgoing=True, pattern=r"\.صيد (.*)"))
+    else:
+        
+@turbo.on(events.NewMessage(outgoing=True, pattern=r"صيد"))
 async def _(event):
     if ispay2[0] == "yes":
         isclaim.clear()
         isclaim.append("on")
         msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 2)
-        ch = str(msg[2])
-        choice = str(msg[1])
+        ch = str(msg[1])
+        choice = str(msg[0])
         trys = 0
-        await event.edit(f"حسناً سأفحص نوع `{choice}` من اليوزرات على `{ch}` , بعدد `{msg[0]}` من المحاولات !")
+        await event.edit(f"**Ok I will check  `{choice}` from the user  `{ch}`**")
 
-        @sython.on(events.NewMessage(outgoing=True, pattern=r"\.حالة الصيد"))
+        @turbo.on(events.NewMessage(outgoing=True, pattern=r"الصيد"))
         async def _(event):
             if ispay2[0] == "yes":
                 if "on" in isclaim:
-                    await event.edit(f"الصيد وصل لـ({trys}) من المحاولات")
+                    await event.edit(f"The examination has arrived **({trys})** of attempts")
                 elif "off" in isclaim:
-                    await event.edit("لايوجد صيد شغال !")
+                    await event.edit("There is NO working check !")
                 else:
-                    await event.edit("خطأ")
+                    await event.edit("mistake")
             else:
                 pass
-        for i in range(int(msg[0])):
+        for i in range(818181):
             if ispay2[0] == 'no':
                 break
             username = ""
@@ -245,35 +273,36 @@ async def _(event):
             t.join()
             isav = que.get()
             if "Available" in isav:
-                await asyncio.sleep(0.0)
+                await asyncio.sleep(1)
                 try:
-                    await sython(functions.channels.UpdateUsernameRequest(
+                    await turbo(functions.channels.UpdateUsernameRequest(
                         channel=ch, username=username))
-                    await event.client.send_message(event.chat_id, f'''
-ᴛʜᴇ ᴋɪɴɢ ᴏғ ʀᴀᴇѕ 🍺 
- - - - - - - - - - - -  
-  ‹𓏺  . FIGHT,َِFOR َِYOUR َِdream .
-
-ᴜѕᴇʀɴᴀᴍᴇ : ❲ (@{username}) ❳‌‌  ⚚
-
-ᴛɪᴍᴇ : {current_datetime}
-
-ѕᴀᴠᴇ : ❲ ᴄʜᴀɴɴᴇʟ ❳‌‌  ⚚ 
-
- ʙʏ : ❲ @xX_Raes  :  @PR_RRR ❳‌‌  ⚚
-    ''')
+                    await turbo.send_file(event.chat_id, 'https://t.me/jc_ksa/17', caption=f'''Leader : @AbnBashar
+User ⤷ @{username}
+Save ⤷ ch
+Clicks ⤷ {trys}
+turbo ⤷ @turboBH
+''')
+                    await turbo.send_file("i5tbot", 'https://t.me/jc_ksa/17', caption=f'''Leader : @AbnBashar
+User ⤷ @{username}
+Save ⤷ Flod - turbo
+Clicks ⤷ {trys}
+turbo ⤷ @turboBH
+''')
                     break
                 except telethon.errors.rpcerrorlist.UsernameInvalidError:
                     with open("banned.txt", "a") as f:
                         f.write(f"\n{username}")
                 except Exception as eee:
-                    await sython.send_message(event.chat_id, f'''خطأ مع {username}
-    الخطأ :
-    {str(eee)}''')
+                    msg = await turbo.send_message(event.chat_id, f'''error with {username}
+                    {str(eee)}''')
+                    if "The username is already taken (caused by UpdateUsernameRequest)" in msg: 
+                        N = datetime.datetime.now()
+                        await event.edit(f"{username} خاصيه \n الوقت هو {N}") 
                     if "A wait of" in str(eee):
                         break
                     else:
-                        await sython.send_message(event.chat.id, " خاصيه 🔥وصلت !")
+                        await turbo.send_message(event.chat.id, "")
             else:
                 pass
             trys += 1
@@ -281,30 +310,23 @@ async def _(event):
         isclaim.clear()
         isclaim.append("off")
         trys = ""
-        await event.client.send_message(event.chat_id, "! 😔انتهى الصيد")
+        await event.client.send_message(event.chat_id, "")
+    else:
+        await event.edit("")
         
-@sython.on(events.NewMessage(outgoing=True, pattern=r"\.تثبيت (.*)"))
+@turbo.on(events.NewMessage(outgoing=True, pattern=r"تثبيت"))
 async def _(event):
     if ispay2[0] == "yes":
         trys = 0
         msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
-        if msg[0] == "تلقائي":  # تثبيت تلقائي عدد يوزر قناة
+        if msg[0] == "قناة":
             isauto.clear()
             isauto.append("on")
             msg = ("".join(event.text.split(maxsplit=2)[2:])).split(" ", 2)
-            username = str(msg[2])
-            ch = str(msg[1])
-            await event.edit(f"حسناً سأحاول تثبيت `{username}` على `{ch}` , بعدد `{msg[0]}` من المحاولات !")
-
-            @sython.on(events.NewMessage(outgoing=True, pattern=r"\.حالة التثبيت التلقائي"))
-            async def _(event):
-                if "on" in isauto:
-                    msg = await event.edit(f"التثبيت وصل لـ({trys}) من المحاولات")
-                elif "off" in isauto:
-                    await event.edit("لايوجد تثبيت شغال !")
-                else:
-                    await event.edit("خطأ")
-            for i in range(int(msg[0])):
+            username = str(msg[1])
+            ch = str(msg[0])
+            await turbo.send_message(event.chat_id, f"Started successfully → `{ch}`")
+            for i in range(15000000):
                 if ispay2[0] == 'no':
                     break
                 t = Thread(target=lambda q, arg1: q.put(
@@ -314,76 +336,27 @@ async def _(event):
                 isav = que.get()
                 if "Available" in isav:
                     try:
-                        await sython(functions.channels.UpdateUsernameRequest(
+                        await turbo(functions.channels.UpdateUsernameRequest(
                             channel=ch, username=username))
-                        await event.client.send_message(event.chat_id, f'''
-                        ᴛʜᴇ ᴋɪɴɢ ᴏғ ʀᴀᴇѕ 🍺 
- - - - - - - - - - - -  
-  ‹𓏺  . FIGHT,َِFOR َِYOUR َِdream .
-
-ᴜѕᴇʀɴᴀᴍᴇ : ❲ (@{username}) ❳‌‌  ⚚
-
-ᴛɪᴍᴇ : {current_datetime}
-
-ѕᴀᴠᴇ : ❲ ᴄʜᴀɴɴᴇʟ ❳‌‌  ⚚ 
-
- ʙʏ : ❲ @xX_Raes  :  @PR_RRR ❳‌‌  ⚚ 
-  ''')
+                        trys += 12
+                        await turbo.send_file(event.chat_id, 'https://t.me/jc_ksa/17', caption=f'''Leader : @AbnBashar
+User ⤷ @{username}
+Save ⤷ Channel
+Clicks ⤷ {trys}
+turbo ⤷ @turboBH''')
                         break
                     except telethon.errors.rpcerrorlist.UsernameInvalidError:
-                        await event.client.send_message(event.chat_id, f"مبند `{username}` ❌❌")
-                        break
+                        await event.client.send_message(event.chat_id, f"band the @{username} ")
                     except Exception as eee:
-
-                        await sython.send_message(event.chat_id, f'''خطأ مع {username}
-    الخطأ :
-    {str(eee)}''')
-                        if "A wait of" in str(eee):
-                            break
-                else:
+                    	await turbo.send_message(event.chat_id, f'''{username}
+- {str(eee)} -''')
+                    if "A wait of" in str(eee):
+                        break
+                else: 
                     pass
                 trys += 1
 
-                await asyncio.sleep(8)
             trys = ""
-            isclaim.clear()
-            isclaim.append("off")
-            await sython.send_message(event.chat_id, "تم الانتهاء من التثبيت التلقائي")
-        if msg[0] == "يدوي":  # تثبيت يدوي يوزر قناة
-            await event.edit(f"حسناً سأحاول تثبيت `{username}` على `{ch}` !")
-            msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
-            username = str(msg[0])
-            ch = str(msg[1])
-            try:
-                await sython(functions.channels.UpdateUsernameRequest(
-                    channel=ch, username=username))
-                await event.client.send_message(event.chat_id, f'''
-ᴛʜᴇ ᴋɪɴɢ ᴏғ ʀᴀᴇѕ 🍺 
- - - - - - - - - - - -  
-  ‹𓏺  . FIGHT,َِFOR َِYOUR َِdream .
-
-ᴜѕᴇʀɴᴀᴍᴇ : ❲ (@{username}) ❳‌‌  ⚚
-
-ᴛɪᴍᴇ : {current_datetime}
-
-ѕᴀᴠᴇ : ❲ ᴄʜᴀɴɴᴇʟ ❳‌‌  ⚚ 
-
- ʙʏ : ❲ @xX_Raes  :  @PR_RRR ❳‌‌  ⚚ 
- 
-  ''')
-            except telethon.errors.rpcerrorlist.UsernameInvalidError:
-                await event.client.send_message(event.chat_id, f"مبند `{username}` ❌❌")
-            except Exception as eee:
-                await sython.send_message(event.chat_id, f'''خطأ مع {username}
-    الخطأ :
-    {str(eee)}''')
-Threads=[] 
-for t in range(200):
-    x = threading.Thread(target=_)
-    le = threading.Thread(target=gen_user)
-    x.start()
-    le.start()
-    Threads.append(x)
-    Threads.append(le)
-for Th in Threads:
-    Th.join()
+            isauto.clear()
+            isauto.append("off")
+            await turbo.send_message(event.chat_id, "done ⤷ 1")
